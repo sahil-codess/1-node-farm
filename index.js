6beb956705286c896fb66761299f03f1f95a9629
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-09 21:49:00 +0430

    created my own web server using http module that handles requests and responses

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
+const http = require('http');
 
+///////////////////////////////////////////////////////////
+// File Reading and Writing
 
 ///// Blocking or synchrounous way
 // const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -10,17 +13,32 @@ const fs = require('fs');
 
 
 //// Non-blocking or asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8' ,(err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8' ,(err, data2) => {
-        console.log(data2)
-        fs.readFile('./txt/append.txt', 'utf-8' ,(err, data3) => {
-            console.log(data3);
+// fs.readFile('./txt/start.txt', 'utf-8' ,(err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8' ,(err, data2) => {
+//         console.log(data2)
+//         fs.readFile('./txt/append.txt', 'utf-8' ,(err, data3) => {
+//             console.log(data3);
 
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('Your files has been written! 😁')
-            })
-        })
-    })
-})
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('Your files has been written! 😁')
+//             })
+//         })
+//     })
+// })
 
-console.log('Will read this!')
+// console.log('Will read this!')
+
+
+///////////////////////////////////////////////////////////////////////////////////
+///// Server
+
+const server = http.createServer((req, res) => {
+    res.end('Hello from ur own web server');
+});
+
+server.listen('8000', '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
+
+
+
