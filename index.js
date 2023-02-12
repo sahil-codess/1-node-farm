a88b9143e982ca45cccbed2d84638e87cdd2108a
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-12 12:59:05 +0430

    asynchronous way of reading files is much better because it does not block the afterwards lines of code until its execution like synchrounous way does, hence it reads the file in the background and the rest of codes are being executed

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 
-const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textInput)
 
-const textOutput = `This is what we know about Avocados: ${textInput} .\n Created on ${Date.now()}`
+///// Blocking or synchrounous way
+// const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textInput)
+// const textOutput = `This is what we know about Avocados: ${textInput} .\n Created on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOutput);
+// console.log('File has been written!')
 
-fs.writeFileSync('./txt/output.txt', textOutput);
 
-console.log('File has been written!')
+//// Non-blocking or asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8' ,(err, data) => {
+    console.log(data);
+})
+
+console.log('Will read this!')
