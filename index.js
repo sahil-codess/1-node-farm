7a70122417a9a572b540ea5bca58ac36ccac674a
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-12 13:28:44 +0430

    multiple callback writings in asynchrounous way

--- index.js
+++ index.js
@@ -10,8 +10,17 @@ const fs = require('fs');
 
 
 //// Non-blocking or asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8' ,(err, data) => {
-    console.log(data);
+fs.readFile('./txt/start.txt', 'utf-8' ,(err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8' ,(err, data2) => {
+        console.log(data2)
+        fs.readFile('./txt/append.txt', 'utf-8' ,(err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your files has been written! 😁')
+            })
+        })
+    })
 })
 
 console.log('Will read this!')
