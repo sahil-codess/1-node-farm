7224e932402184a7d857449d8aebbfe11690e75e
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-12 10:28:55 +0430

    writing files with writeFileSync function

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
 
-const textinput = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textinput)
+const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textInput)
+
+const textOutput = `This is what we know about Avocados: ${textInput} .\n Created on ${Date.now()}`
+
+fs.writeFileSync('./txt/output.txt', textOutput);
+
+console.log('File has been written!')
