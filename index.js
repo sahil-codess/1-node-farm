76769e903e9f7d95ed003e563d98a6309de95dbb
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-13 10:45:29 +0430

    getting data from our api and sending it to browser also turning json data to javascript object

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ const server = http.createServer((req, res) => {
         res.end('The overview page');
     } else if (pathName === '/product') {
         res.end('This is the product page')
+    } else if (pathName === '/api') {
+        fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+            const productData = JSON.parse(data)
+            res.writeHead(200, { 'Content-type': 'application/json' })
+            res.end(data)
+        })
+
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html'
