a917f7058d57f0551dedecc64f82405b75288307
Sahilkhan <sahilkhanahmadzai4@gmail.com>
2023-02-14 11:47:08 +0430

    installing prettier and npm features

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const http = require('http');
-const { default: slugify } = require('slugify');
-const url = require('url')
+const fs = require("fs");
+const http = require("http");
+const { default: slugify } = require("slugify");
+const url = require("url");
 
 ///////////////////////////////////////////////////////////
 // File Reading and Writing
@@ -12,7 +12,6 @@ const url = require('url')
 // const textOutput = `This is what we know about Avocados: ${textInput} .\n Created on ${Date.now()}`
 // fs.writeFileSync('./txt/output.txt', textOutput);
 // console.log('File has been written!')
-
 
 //// Non-blocking or asynchronous way
 // fs.readFile('./txt/start.txt', 'utf-8' ,(err, data1) => {
@@ -30,69 +29,78 @@ const url = require('url')
 
 // console.log('Will read this!')
 
-
 ///////////////////////////////////////////////////////////////////////////////////
 ///// Server
 
-
 //// synchronous code because there is no need to read data every time the switches to api rout
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 
 const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
 
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-const dataObj = JSON.parse(data)
+FIXME;
 
-const slugs = dataObj.map(el => slugify(el.productName, {lower: true}))
-console.log(slugs)
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
-    const { query, pathname } = url.parse(req.url, true);
-    // overview page
-    if (pathname === '/' || pathname === '/overview') {
-        res.writeHead(200, { 'Content-type': 'text/html' })
-        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-        res.end(output);
+  const { query, pathname } = url.parse(req.url, true);
+  // overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
 
-        // product page
-    } else if (pathname === '/product') {
-        res.writeHead(200, { 'Content-type': 'text/html' })
-        const product = dataObj[query.id]
-        const output = replaceTemplate(tempProduct, product);
-        res.end(output)
+    // product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
-        // api page
-    } else if (pathname === '/api') {
-        res.writeHead(200, { 'Content-type': 'application/json' })
-        res.end(data)
+    // api page
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
 
-        // 404 page
-    } else {
-        res.writeHead(404, {
-            'Content-type': 'text/html'
-        })
-        res.end('<h1>Page not found!</h1>')
-    }
+    // 404 page
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 
-server.listen('8000', '127.0.0.1', () => {
-    console.log('Listening to requests on port 8000');
+server.listen("8000", "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
 });
-
-
-
